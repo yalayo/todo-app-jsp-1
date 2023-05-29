08f6a48fae11c7e3400a8d9fdc4efe2f961140fd
atyourr3ach <2856460-atyourr3ach@users.noreply.replit.com>
2023-05-29 15:00:01 +0000

    Working on login feature.

---
+++ src/main/java/com/rondonsarnik/app/dao/LoginDao.java
@@ -0,0 +1,35 @@
+package com.rondonsarnik.app.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.rondonsarnik.app.model.LoginBean;
+import com.rondonsarnik.app.utils.JDBCUtils;
+
+public class LoginDao {
+
+	public boolean validate(LoginBean loginBean) throws ClassNotFoundException {
+		boolean status = false;
+
+		Class.forName("com.mysql.jdbc.Driver");
+
+		try (Connection connection = JDBCUtils.getConnection();
+				// Step 2:Create a statement using connection object
+				PreparedStatement preparedStatement = connection
+						.prepareStatement("select * from users where username = ? and password = ? ")) {
+			preparedStatement.setString(1, loginBean.getUsername());
+			preparedStatement.setString(2, loginBean.getPassword());
+
+			System.out.println(preparedStatement);
+			ResultSet rs = preparedStatement.executeQuery();
+			status = rs.next();
+
+		} catch (SQLException e) {
+			// process sql exception
+			JDBCUtils.printSQLException(e);
+		}
+		return status;
+	}
+}
